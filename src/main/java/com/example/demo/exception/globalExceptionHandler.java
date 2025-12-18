a5cee2ac7896c1cea895824a56f2de29e5740e3f
bala-amypo <bala@amypo.in>
2025-12-18 09:29:17 +0000

    User commit

--- src/main/java/com/example/demo/exception/globalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/globalExceptionHandler.java
@@ -8,7 +8,7 @@ import com.example.demo.exception.*;
 @RestControllerAdvice
 public class globalExceptionHandler{
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map>handleFieldError(MethodArgumentNotValidException ex){
+    public ResponseEntity<Map<String,String>>handleFieldError(MethodArgumentNotValidException ex){
         Map<String,String> errors = new HashMap<>();
         ex.getBindingResult().getFieldErrors().forEach(err -> errors.put(err.getField(),err.getDefaultMessage()));
         return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
